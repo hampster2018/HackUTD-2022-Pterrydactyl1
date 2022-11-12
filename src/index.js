542a79dcb01d7b6e2920a9ab6e9b336e6d6b8868
Eric Shields <shieldse01@yahoo.com>
2022-11-12 15:35:48 -0600

    initial OAuth push

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import OauthLogin from './OAuth';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <OauthLogin/>
   </React.StrictMode>
 );
